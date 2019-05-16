642332882d7f7b2f604c48945b0965adb373e717
Maung Pauk <maungpauk@gmail.com>
2019-05-16 23:31:06 +0630

    Update sfl2.js

--- sfl2.js
+++ sfl2.js
@@ -18,16 +18,11 @@ var a_to_ve="";var a_to_vf=new Array();var a_to_vg=0;a_to_ve=document.getElement
 {a_to_vh=true;}
 j++;}
 if(a_to_vh==false)
-{var encryptedUrl=Base64.encode(a_to_vi);
-var x=Math.floor((Math.random()*4)+ 1);
-var xxx=null;
-if(x=="1"){xxx="basic-reason-you-need-insurance.html"}
-if(x=="2"){xxx="type-function-insurance-beginners.html"}
-if(x=="3"){xxx="tips-avoid-life-insurance-mistake.html"}
-if(x=="4"){xxx="best-ways-decrease-insurance-cost.html"}a_to_ve[i].href="https://safe.iweblogsite.com/2018/11/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+{var encryptedUrl=Base64.encode(a_to_vi);a_to_ve[i].href="https://kompisafe.blogspot.com/p/go-to-url.html?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
 {a_to_vk.innerHTML+=a_to_va;}}
 function a_to_fa()
 {var a_to_vf=new Array();protected_links=protected_links.replace(" ","");a_to_vf=protected_links.split(",");return a_to_vf;}
+auto_safelink();
